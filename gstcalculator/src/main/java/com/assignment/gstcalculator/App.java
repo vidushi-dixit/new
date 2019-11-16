1c767e5ff0b4e8655f55ff6914c71adcad1b8098
Dell <Dell@192.168.1.20>
2019-11-17 00:08:28 +0530

    calculate change

--- gstcalculator/src/main/java/com/assignment/gstcalculator/App.java
+++ gstcalculator/src/main/java/com/assignment/gstcalculator/App.java
@@ -1,13 +1,34 @@
 package com.assignment.gstcalculator;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-    	
+    	String input="yes";
+		ProductFactory productFactory=new ProductFactory();
+		Scanner sc = new Scanner(System.in);
+		while(input.equalsIgnoreCase("yes"))
+		 {
+			 System.out.print("Enter unit:");
+			 int unit = sc.nextInt();
+			 System.out.print("Enter product name:");
+			 String productName = sc.next();
+			 System.out.print("Enter price per unit:");
+			 double price = sc.nextDouble();
+			 Product product= productFactory.getProduct(productName);
+			 if(null==product)
+				 System.out.println("No such product exists");
+			 else
+			 {
+				 double[] gst=product.calculateGst(price, unit);
+				 System.out.println("GST aplicable per unit:"+gst[0]);
+				 System.out.println("Final Price:"+gst[1]);
+			 }
+			 System.out.print("Enter yes to continue:");
+			 input=sc.next();
+		 }
+		 sc.close();
     }
 }
--- gstcalculator/src/main/java/com/assignment/gstcalculator/Product.java
+++ gstcalculator/src/main/java/com/assignment/gstcalculator/Product.java
@@ -7,8 +7,8 @@ public class Product
 	public double[] calculateGst(double price,int unit)
 	{	
 		double[] ans = new double[2]; 
-        ans[0] =unit*price*(this.interest/100);
-        ans[1] = (unit*price)+ans[0]; 
+        ans[0] =price*(this.interest/100);
+        ans[1] = unit*(price+ans[0]); 
         return ans; 
 	}
 }
